53898b4cfa048ff6446e367a85d514766509fbfc
mlc-kawabata-shohei <kawabata-shohei@mitsue.co.jp>
2024-12-23 12:10:20 +0900

    minorfix

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./.amplify-hosting
 
-#mkdir -p ./.amplify-hosting/compute
+mkdir -p ./.amplify-hosting/compute
 
 #cp -r ./dist ./.amplify-hosting/compute/default
 #cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
